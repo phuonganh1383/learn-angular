f86e64d6b331b74a7cbf994f4040441a0bdc8cc4
anhh1 <anhntp@itsj-group.com>
2021-08-17 17:14:49 +0700

    create new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {MultiTranslateHttpLoader} from "ngx-translate-multi-http-loader";
+
+export function createTranslateLoader(http : HttpClient) {
+  // return new TranslateHttpLoader(http, './assets/i18n/', '.json')
+  return new MultiTranslateHttpLoader(http,[
+    {prefix: "./assets/i18n/", suffix: ".json"},
+    {prefix: "./assets/i18n/validate/", suffix: ".json"},
+  ]);
+}
 
 @NgModule({
   declarations: [
@@ -10,6 +22,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({loader: {provide: TranslateLoader,
+      useFactory: createTranslateLoader, deps: [HttpClient]}}),
     AppRoutingModule
   ],
   providers: [],
